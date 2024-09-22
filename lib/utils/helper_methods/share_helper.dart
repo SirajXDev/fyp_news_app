748ac60ea207f97acb7ec424692a9e1b48d85bd5
imziaafridi <imziaafridi@gmail.com>
2024-09-22 13:03:07 +0500

    share logic updates

--- lib/utils/helper_methods/share_helper.dart
+++ lib/utils/helper_methods/share_helper.dart
@@ -1,21 +1,20 @@
- import 'package:flutter/material.dart';
- import 'package:share_it/share_it.dart';
+import 'package:flutter/material.dart';
+import 'package:share_it/share_it.dart';
 
- class ShareHelper {
-   /// Shares text and url
+class ShareHelper {
+  /// Shares text and url
 
-   static Future<void> shareLink({
-     required String url,
+  static Future<void> shareLink({
+    required String url,
     required String? title,
   }) async {
-     try {
-       await ShareIt.link(
-         url: url,
-         androidSheetTitle: title,
-       );
-     } catch (e) {
-       debugPrint('Error sharing link: $e');
-     }
+    try {
+      await ShareIt.link(
+        url: url,
+        androidSheetTitle: title ?? 'Share News With Friends...',
+      );
+    } catch (e) {
+      debugPrint('Error sharing link: $e');
+    }
   }
- }
-
+}
